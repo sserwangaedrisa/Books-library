035a3617f8baf292440ab47ae74ff50fa5c80a42
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-09-02 15:44:17 +0300

    Using the object methods

--- style.js
+++ style.js
@@ -1,53 +1,65 @@
+const remove = document.getElementsByClassName("removeButton");
+const add = document.getElementById("addingButton");
+const listItem = document.getElementsByClassName("list");
+const bookSection = document.getElementById("ul");
+var Title = document.getElementById("Title");
+var Author = document.getElementById("Author");
+const BooksArray = [];
 
-var remove= document.getElementsByClassName('removeButton')
-var add= document.getElementById('addingButton')
-var listItem=document.getElementsByClassName('list')
-var bookSection=document.getElementById('ul')
-
-
-// for (var i=0;i<remove.length;i++){
-//     remove[i].addEventListener('click', function(event){
-//         // console.log(Array.from(remove).indexOf(event.target))
-//         var listIndex=Array.from(remove).indexOf(event.target)
-//         listItem[listIndex].parentNode.removeChild(listItem[listIndex])
-//     })
-// }
-var title=document.getElementById('Title')
-var author=document.getElementById('Author')
-
-add.addEventListener('click', function(){
-    // console.log(title.value)
-    if (title.value.length !==0){
-        var newList=document.createElement('div')
-        var newTitle=document.createElement('div')
-        var newAuthor=document.createElement('div')
-        var newdivButton=document.createElement('div')
-        newdivButton.classList.add('removed')
-        var newButton= document.createElement('button')
-        newButton.classList.add('removeButton')
-        newButton.textContent='Remove'
-        newTitle.appendChild(document.createTextNode(`" ${title.value} " by   `))
-        newAuthor.appendChild(document.createTextNode(`${author.value} `))
-        newAuthor.style.padding='7px'
-        newdivButton.appendChild(newButton)
-        newList.appendChild(newTitle)
-        newList.appendChild(newAuthor)
-        newList.appendChild(newdivButton)
-        newList.classList.add('minorList')
-        bookSection.appendChild(newList)
-        title.value=''
-        author.value=''
-        }     
-})
-
-
-bookSection.addEventListener('click', function(event) {
-    if (event.target.classList.contains('removeButton')) {
-        var listItem = event.target.closest('.minorList');
-        listItem.parentNode.removeChild(listItem);
+add.addEventListener("click", function (event) {
+  class NewBook {
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
     }
+    updateBooksArray() {
+      BooksArray.push(this);
+    }
+  }
+  class Child extends NewBook {
+    constructor(title, author) {
+      super(title, author);
+      this.title = title;
+      this.author = author;
+    }
+
+    newBookItem() {
+      if (Title.length !== 0) {
+        const newList = document.createElement("div");
+        const newTitle = document.createElement("div");
+        const newAuthor = document.createElement("div");
+        const newdivButton = document.createElement("div");
+        newdivButton.classList.add("removed");
+        const newButton = document.createElement("button");
+        newButton.classList.add("removeButton");
+        newButton.textContent = "Remove";
+        newTitle.appendChild(
+          document.createTextNode(`" ${this.title} " by   `)
+        );
+        newAuthor.appendChild(document.createTextNode(`${this.author} `));
+        newAuthor.style.padding = "7px";
+        newdivButton.appendChild(newButton);
+        newList.appendChild(newTitle);
+        newList.appendChild(newAuthor);
+        newList.appendChild(newdivButton);
+        newList.classList.add("minorList");
+        bookSection.appendChild(newList);
+        Title.value = "";
+        Author.value = "";
+      }
+    }
+  }
+
+  var book = new Child(Title.value, Author.value);
+
+  book.newBookItem();
+  book.updateBooksArray();
+  console.log(BooksArray);
 });
 
-
-
-
+bookSection.addEventListener("click", function (event) {
+  if (event.target.classList.contains("removeButton")) {
+    var listItem = event.target.closest(".minorList");
+    listItem.parentNode.removeChild(listItem);
+  }
+});
