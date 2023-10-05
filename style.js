ff5943aaa0e5528809ab22e8929c0cab88b9e84d
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-10-05 12:20:31 +0300

    Modifying the storage of the website to use local storage of the user computer

--- style.js
+++ style.js
@@ -1,63 +1,62 @@
-const add = document.getElementById('addingButton');
-const bookSection = document.getElementById('ul');
-const Title = document.getElementById('Title');
-const Author = document.getElementById('Author');
-const BooksArray = [];
-add.addEventListener('click', () => {
-  class NewBook {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-    }
+const title = document.getElementById('Title');
+const author = document.getElementById('Author');
+const addButton = document.getElementById('AddButton');
+const booksSection = document.getElementById('Books-section');
+const addedBooks = [];
 
-    updateBooksArray() {
-      if (this.title.length !== 0) {
-        BooksArray.push(this);
-      }
-    }
+const addingBooksFunction = () => {
+  if (title.value.length !== 0) {
+    const addedBooks = JSON.parse(localStorage.getItem('Added books'));
+    addedBooks.push({ Title: title.value, Author: author.value });
+    localStorage.setItem('Added books', JSON.stringify(addedBooks));
   }
-  class Child extends NewBook {
-    constructor(title, author) {
-      super(title, author);
-      this.title = title;
-      this.author = author;
+};
+
+const cleaningEntries = () => {
+  title.value = '';
+  author.value = '';
+};
+
+const removingBookFunction = (id) => {
+  const booksArray = JSON.parse(localStorage.getItem('Added books'));
+  const updatedArray = booksArray.filter((book) => book.Title !== id);
+
+  localStorage.setItem('Added books', JSON.stringify(updatedArray));
+};
+
+if (localStorage.getItem('Added books') === null) {
+  localStorage.setItem('Added books', JSON.stringify(addedBooks));
+} else {
+  const addedBook = JSON.parse(localStorage.getItem('Added books'));
+  addedBook.forEach((book, index) => {
+    const newBookRow = document.createElement('div');
+    newBookRow.classList.add('newRow');
+    if (index % 2 === 0) {
+      newBookRow.classList.add('evenBook');
     }
+    const newBook = document.createElement('p');
+    newBook.textContent = `"${book.Title}" by ${book.Author}`;
+    newBook.classList.add('newBook');
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.classList.add('Remove');
+    removeBtn.setAttribute('id', book.Title);
 
-    newBookItem() {
-      if (this.title.length !== 0) {
-        const newList = document.createElement('div');
-        const newTitle = document.createElement('div');
-        const newAuthor = document.createElement('div');
-        const newdivButton = document.createElement('div');
-        newdivButton.classList.add('removed');
-        const newButton = document.createElement('button');
-        newButton.classList.add('removeButton');
-        newButton.textContent = 'Remove';
-        newTitle.appendChild(
-          document.createTextNode(`' ${this.title} ' by   `),
-        );
-        newAuthor.appendChild(document.createTextNode(`${this.author} `));
-        newAuthor.style.padding = '7px';
-        newdivButton.appendChild(newButton);
-        newList.appendChild(newTitle);
-        newList.appendChild(newAuthor);
-        newList.appendChild(newdivButton);
-        newList.classList.add('minorList');
-        bookSection.appendChild(newList);
-        Title.value = '';
-        Author.value = '';
-      }
-    }
+    removeBtn.addEventListener('click', (event) => {
+      removingBookFunction(event.target.id);
+      window.location.reload();
+    });
+
+    newBookRow.appendChild(newBook);
+    newBookRow.appendChild(removeBtn);
+    booksSection.appendChild(newBookRow);
+  });
+}
+
+addButton.addEventListener('click', () => {
+  if (title.value.length !== 0) {
+    addingBooksFunction();
   }
-
-  const book = new Child(Title.value, Author.value);
-  book.newBookItem();
-  book.updateBooksArray();
-});
-
-bookSection.addEventListener('click', (event) => {
-  if (event.target.classList.contains('removeButton')) {
-    const listItem = event.target.closest('.minorList');
-    listItem.parentNode.removeChild(listItem);
-  }
+  cleaningEntries();
+  window.location.reload();
 });
